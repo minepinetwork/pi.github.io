2ca3fab7afcf54e59ae02ac0f368d4cded05f926
minepinetwork <pitrader.buysell@gmail.com>
2023-06-08 12:11:49 +0530

    secomd change b

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "efc7c939c223512fdd2ef8420aa9560b",
-"/": "efc7c939c223512fdd2ef8420aa9560b",
-"main.dart.js": "d1eeee605e8db7fa2a31026e14d51300",
+"index.html": "14b8a3286f7a4bf494c009106a60b17d",
+"/": "14b8a3286f7a4bf494c009106a60b17d",
+"main.dart.js": "a4a4fd62048e4800632cb728d9000031",
 "manifest.json": "bbf1db930c12447169cb5a37bbc9beca",
 "version.json": "bfe7e3a8ff90c7dbe6eb9af70fb61215"
 };
